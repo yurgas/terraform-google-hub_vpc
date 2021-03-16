ff7a79e74a6d0f26930fe16eccbfd13dacd4bf11
yurgas <yurarsit@gmail.com>
2021-03-16 12:30:28 +0300

    Add outputs description

--- output.tf
+++ output.tf
@@ -1,7 +1,9 @@
 output "vpc_id" {
+  description = "Hub VPC id"
   value = google_compute_network.vpc.id
 }
 
 output "subnet_id" {
+  description = "Subnet id in hub VPC"
   value = google_compute_subnetwork.subnet.id
 }
