cc74f4b08ba7eba17bc1ff29b30aa006282e1cd3
yurgas <yurarsit@gmail.com>
2021-03-16 12:21:52 +0300

    Add description

--- variables.tf
+++ variables.tf
@@ -1,21 +1,26 @@
 variable "region" {
+  description = "Default region for module resources"
   type    = string
   default = "us-central1"
 }
 
 variable "zone" {
+  description = "Devault zone for module resources"
   type    = string
   default = "us-central1-a"
 }
 
 variable "project_id" {
+    description = "Project_id for resources"
     type = string
 }
 
 variable "vpc_name" {
+    description = "HUB VPC name"
   type = string
 }
 
 variable "subnet_range" {
+    description = "Subnet address to be created in VPC"
   type = string
 }
